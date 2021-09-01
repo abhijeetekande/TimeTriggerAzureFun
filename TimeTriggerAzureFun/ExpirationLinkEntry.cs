0252ec9f7d8b1214567746d1c4371284ea0ed1c9
abhijeetekande <abhi.ekande@gmail.com>
2021-09-01 16:59:14 +0000

    changes to permissions and entry

---
+++ TimeTriggerAzureFun/ExpirationLinkEntry.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace TimeTriggerAzureFun
+{
+    public static class ExpirationLinkEntry
+    {
+        [FunctionName("ExpirationLinkEntry")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Table("tblExpirationLinks", Connection = "AzureWebJobsStorage")] CloudTable cloudTable,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            try
+            {
+                
+                log.LogInformation($"{String.Concat(Convert.ToString(data.targetUsers).Split('@')[0], Convert.ToString(data.ItemURL))}");
+                TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(new ExpirationLinksTableEntity()
+                {
+                    PartitionKey = Guid.NewGuid().ToString(),// Convert.ToString(data.currentUser),  
+                    RowKey = Guid.NewGuid().ToString(), //String.Concat(Convert.ToString(data.targetUsers).Split('@')[0], Convert.ToString(data.ItemURL)),// Guid.NewGuid().ToString(),//$"{Convert.ToString(data.targetUsers).Split('@')[0]}{data.ItemURL}",
+                    ItemURL = data.ItemURL,
+                    SharedByUser = data.currentUser,
+                    SharedWithUser = data.targetUsers,
+                    PermissionLevel = Convert.ToBoolean(data.editingEnabled) ? "Edit" : "Read",
+                    ExpirationDate = data.expirationDate,
+                    WebURL = data.webUrl,
+                    Expired = Convert.ToBoolean(false),
+                    SiteID = data.SiteID,
+                    ListID = data.ListID,
+                    ItemID = data.ItemID,
+                    PermissionID = data.PermissionID
+
+                });
+                TableResult result = cloudTable.Execute(insertOrMergeOperation);
+                       
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation($"Error: {ex.StackTrace}");
+            }
+
+            string responseMessage = $"Hello, {data.currentUser}. your shared link entered in azure table.";
+            log.LogInformation($"C# Site:");
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        public class ExpirationLinksTableEntity : TableEntity
+        {
+            public string ItemURL { get; set; }
+            public string SharedByUser { get; set; }
+            public string SharedWithUser { get; set; }
+            public string PermissionLevel { get; set; }
+            public string ExpirationDate { get; set; }
+            public string WebURL { get; set; }
+            public Boolean Expired { get; set; }
+            public string SiteID { get; set; }
+            public string ListID { get; set; }
+            public string ItemID { get; set; }
+            public string PermissionID { get; set; }
+
+        }
+    }
+}
